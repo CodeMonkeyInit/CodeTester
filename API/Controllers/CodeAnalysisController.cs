68a609310082af5be48cbccb7b8f47b188ef530d
CodeMonkeyInit <kyliev09@gmail.com>
2019-07-06 13:01:07 +0300

    Added production config and deployment path

--- API/Controllers/CodeAnalysisController.cs
+++ API/Controllers/CodeAnalysisController.cs
@@ -9,24 +9,23 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [Route("api/[controller]")]
-         [ApiController]
-         public class CodeAnalysisController : ControllerBase, ICodeAnalysisApi
-         {
-             private readonly CodeAnalyzerFactory _codeAnalyzerFactory;
-     
-             public CodeAnalysisController(CodeAnalyzerFactory codeAnalyzerFactory)
-             {
-                 _codeAnalyzerFactory = codeAnalyzerFactory;
-             }
-     
-             // POST api/values
-             [HttpPost]
-             public async Task<CodeAnalysisResult> AnalyzeCodeAsync([FromBody] TestingCode code)
-             {
-                 CodeAnalyzer codeAnalyzer = _codeAnalyzerFactory.GetCodeAnalyzer(code);
-     
-                 return await codeAnalyzer.Analyse(code);
-             }
-             
-         }
+    [ApiController]
+    public class CodeAnalysisController : ControllerBase, ICodeAnalysisApi
+    {
+        private readonly CodeAnalyzerFactory _codeAnalyzerFactory;
+
+        public CodeAnalysisController(CodeAnalyzerFactory codeAnalyzerFactory)
+        {
+            _codeAnalyzerFactory = codeAnalyzerFactory;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<CodeAnalysisResult> AnalyzeCodeAsync([FromBody] TestingCode code)
+        {
+            CodeAnalyzer codeAnalyzer = _codeAnalyzerFactory.GetCodeAnalyzer(code);
+
+            return await codeAnalyzer.Analyse(code);
+        }
+    }
 }
